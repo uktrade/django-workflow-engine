a5b1ee05fc8a3c6e3ad4e70f264a01fb039696ab
davecharles <22290851+davecharles@users.noreply.github.com>
2021-10-04 13:15:11 +0100

    fix: explicit args.

--- django_workflow_engine/generate_urls.py
+++ django_workflow_engine/generate_urls.py
@@ -26,21 +26,31 @@ Alternatively provide own view classes for flow list and flow view:
 from django.urls import path, include
 
 
-def workflow_urls(**kwargs):
+def workflow_urls(
+        list_view=None,
+        view=None,
+        create_view=None,
+        continue_view=None,
+        diagram_view=None
+):
     """Generate workflow urls.
 
     Generate route config while providing the option for the the package user
     to specify their own view class overrides.
 
-    :param (dict) kwargs: classes to override.
+    :param (View) list_view: Alternative to FlowListView.
+    :param (View) view: Alternative to FlowView.
+    :param (View) create_view: Alternative to FlowCreateView.
+    :param (View) continue_view: Alternative to FlowContinueView.
+    :param (View) diagram_view: Alternative to FlowDiagramView.
     :returns (tuple): result django.urls.include invocation.
     """
-    import django_workflow_engine.views as workflow_views  # Avoid circular deps
-    list_view = kwargs.get("list_view", workflow_views.FlowListView)
-    view = kwargs.get("view", workflow_views.FlowView)
-    create_view = kwargs.get("create_view", workflow_views.FlowCreateView)
-    continue_view = kwargs.get("continue_view", workflow_views.FlowContinueView)
-    diagram_view = kwargs.get("diagram_view", workflow_views.FlowDiagramView)
+    import django_workflow_engine.views as workflow_views  # Avoid circular import
+    list_view = list_view if list_view else workflow_views.FlowListView
+    view = view if view else workflow_views.FlowView
+    create_view = create_view if create_view else workflow_views.FlowCreateView
+    continue_view = continue_view if continue_view else workflow_views.FlowContinueView
+    diagram_view = diagram_view if diagram_view else workflow_views.FlowDiagramView
     return include(
         [
             path("", list_view.as_view(), name="flow-list"),
