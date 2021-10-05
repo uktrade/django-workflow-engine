1ded7b385daf053b38273b7fb3f9fb1a1e2919c1
davecharles <22290851+davecharles@users.noreply.github.com>
2021-10-05 18:52:55 +0100

    feat: refine dynamic workflow import.

--- django_workflow_engine/utils.py
+++ django_workflow_engine/utils.py
@@ -10,20 +10,38 @@ def build_workflow_choices(workflows):
     Builds a choices list by iterating over the workflows dict
     provided.
 
-    :param (dict) workflows: Map of workflow name to full module path of
-        workflow including class e.g:
-        {'onboard_contractor: 'workflows.onboard_contractor.OnboardContractor, ...}
+    :param (list) workflows: List of workflows, module path of workflow
+        including class e.g: ['workflows.onboard_contractor.OnboardContractor, ...]
+    :returns (list[Tuple]): List of tuples (workflow class name, display name)
     """
     choices = []
-    for workflow_name in workflows:
-        workflow_path = workflows[workflow_name]
-        workflow_class = lookup_workflow(workflow_path)
-        choices.append((workflow_class, workflow_name))
+    for workflow_path in workflows:
+        workflow_class = load_workflow(workflow_path)
+        display_name = workflow_class.name.title().replace("_", " ")
+        choices.append((workflow_class.name, display_name))
     return choices
 
 
-def lookup_workflow(workflow_path):
-    """Look up a workflow class.
+def lookup_workflow(workflows, name):
+    """Look up workflow class.
+
+    Given the configured workflows and a workflow name, returns the associated
+    workflow class.
+
+    :param (list) workflows: Configured workflows.
+    :param (str) name: Workflow name.
+    :returns (class): The requested workflow class.
+    :raises (WorkflowImproperlyConfigured): If workflow not found.
+    """
+    for workflow_path in workflows:
+        workflow_class = load_workflow(workflow_path)
+        if workflow_class.name == name:
+            return workflow_class
+    raise WorkflowImproperlyConfigured(f"Cannot find workflow: {name}")
+
+
+def load_workflow(workflow_path):
+    """Load a workflow class.
 
     Given a workflow path, extrapolates the containing package/modules, imports
     it and loads specified class.
