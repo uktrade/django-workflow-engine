14d7799e49942e8289a0d137caea523cb2cb2de2
davecharles <22290851+davecharles@users.noreply.github.com>
2021-10-06 10:49:49 +0100

    fix: bump package version to 0.0.12.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="django-workflow-engine",
-    version="0.0.11",
+    version="0.0.12",
     packages=find_packages(),
     include_package_data=True,
     author="DIT Live Service Team",
