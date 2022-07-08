96751ef65e186e24dcfbf120c9691193eb44973d
Luisella Strona <luisella21@hotmail.com>
2022-07-08 14:16:06 +0100

    Added gateway handling.

--- django_workflow_engine/tests/test_import_bpmn.py
+++ django_workflow_engine/tests/test_import_bpmn.py
@@ -1,12 +1,12 @@
 import pytest
 from django_workflow_engine.import_bpmn import import_BPMN
 
-
+# Not a test, but pytest gives more details when there is an error
+# TODO Add unit tests
 @pytest.mark.django_db
 def test_bpmn_xml_import(settings):
     python_path = "./output/test-manual/simple/broken.py"
     bpmn_path = "./output/test-manual/simple/manually-generated-output_1.xml"
-    # bpmn_path = "./output/test-manual/simple/bpmn_editor_simple_example.xml"
 
     import_BPMN(bpmn_path, python_path)
 
