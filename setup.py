c528e7cb02708755b0d98fa18820da81dba0ce8d
Ross Miller <ross.miller@digital.trade.gov.uk>
2022-08-23 10:43:22 +0100

    Bump Django

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[
-        "django>=3.2.8",
+        "django>=3.2.15",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
