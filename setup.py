acbcbcb32ead0ec1052e54f64365fc964f5e1ffa
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2023-05-04 14:00:51 +0100

    Bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("docs/index.md", "r") as fh:
 
 setup(
     name="django-workflow-engine",
-    version="0.1.0rc7",
+    version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
     author="DIT Live Service Team",
