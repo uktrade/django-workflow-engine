208e87f3e828c12b89173372c320424bcf7fa3ec
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2023-06-07 16:26:06 +0100

    Execute past exceptions (#31)
    * Catch the exceptions but continue to execute the tasks
    
    * Add test to make sure if an error is raised, the "non blocked" tasks still run
    
    * Add more error task tests

--- django_workflow_engine/tests/test_workflows/test_linear_workflow.py
+++ django_workflow_engine/tests/test_workflows/test_linear_workflow.py
@@ -23,3 +23,25 @@ def test_workflow_creation(settings):
 
     task_order = [task_status.step_id for task_status in TaskStatus.objects.all()]
     assert task_order == correct_task_order
+
+
+@pytest.mark.django_db()
+def test_with_error_workflow(settings):
+    linear_workflow.steps[1].task_name = "error_task"
+
+    flow, executor, test_user = set_up_flow(
+        settings,
+        linear_workflow,
+    )
+    executor.run_flow(user=test_user)
+
+    assert TaskStatus.objects.count() == 2
+    assert TaskStatus.objects.filter(done=True).count() == 1
+
+    correct_task_order = [
+        "start",
+        "task_a",
+    ]
+
+    task_order = [task_status.step_id for task_status in TaskStatus.objects.all()]
+    assert task_order == correct_task_order
--- django_workflow_engine/tests/test_workflows/test_split_workflow.py
+++ django_workflow_engine/tests/test_workflows/test_split_workflow.py
@@ -25,3 +25,28 @@ def test_parallel_path_no_join_workflow(settings):
 
     task_order = [task_status.step_id for task_status in TaskStatus.objects.all()]
     assert task_order == correct_task_order
+
+
+@pytest.mark.django_db()
+def test_parallel_path_no_join_with_error_workflow(settings):
+    split_workflow.steps[1].task_name = "error_task"
+
+    flow, executor, test_user = set_up_flow(
+        settings,
+        split_workflow,
+    )
+    executor.run_flow(user=test_user)
+    executor.run_flow(user=test_user)
+
+    assert TaskStatus.objects.count() == 4
+    assert TaskStatus.objects.filter(done=True).count() == 3
+
+    correct_task_order = [
+        "start",
+        "task_a",
+        "task_b",
+        "finish_task_b",
+    ]
+
+    task_order = [task_status.step_id for task_status in TaskStatus.objects.all()]
+    assert task_order == correct_task_order
